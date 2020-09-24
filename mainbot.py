165f36b85f2e56d53890e23392675ecb07e4de8e
samarjeet2 <71706128+samarjeet2@users.noreply.github.com>
2020-09-24 17:36:49 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -30,9 +30,9 @@ def follower_count(user):
 api = create_api()
 
 while True:
-    user = api.get_user('the_ameen_manna')
-    api.update_profile(name=f'ameen|{follower_count(user)} Followers')
-    print(f'Updating Twitter Name : ameen|{follower_count(user)} Followers')
+    user = api.get_user('@Samarje10501741')
+    api.update_profile(name=f'Samarjeet|{follower_count(user)} Followers')
+    print(f'Updating Twitter Name : Samarjeet|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
   
